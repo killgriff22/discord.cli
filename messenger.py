e2c7fbe6369577baca345e86fabd06e3310839c6
griffn <37548204+killgriff22@users.noreply.github.com>
2021-10-06 01:35:07 +0000

    removed files that are unused

--- messenger.py
+++
@@ -1,5 +0,0 @@
-from modules.handler import handler
-from config import config
-import asyncio
-handler.login(config.TOKEN)
-asyncio.run(handler.sendmessage(handler.currentguild,handler.currentchannel,"test123,this is an automated message"))
